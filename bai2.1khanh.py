71c63419286fd8b2004e0ff21b8a914266ce17c6
nvk123009-cloud <nvk123009@gmail.com>
2025-12-09 08:11:18 -0600

    Delete bai2.1khanh.py

--- bai2.1khanh.py
+++
@@ -1,8 +0,0 @@
-print("Sinh vien:Le Dang Anh Cong")
-print("Ma so sv: 245752021610130")
-n1=int(input("enter n1 value"))
-n2=int(input("enter n2 value"))
-if n1>n2:
-    print("n1bsi big")
-else:
-    print("n2 is big")
